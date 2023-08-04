362ae53bf9309b57f9cf0646cda43f692f678f37
deekshith <deekshith@Mac-mini.local>
2023-08-04 20:06:17 +0530

    working input design-system

--- src/components/DisplayArea/DisplayArea.tsx
+++ src/components/DisplayArea/DisplayArea.tsx
@@ -1,18 +1,68 @@
+import Input from "../Input/Input";
 import "./DisplayArea.css";
 
 const DisplayArea = () => {
   return (
     <article>
-      <h1>Buttons</h1>
+      <h1>Inputs</h1>
       <div className="buttons-display">
         <div className="button-set">
-          <h3>Size: small</h3>
-          <div className="button-row"></div>
+          <h3>Size</h3>
+          <div className="button-row">
+            <Input size="sm" />
+            <Input size="md" />
+          </div>
         </div>
-
+        <div className="button-set">
+          <h3>Label</h3>
+          <div className="button-row">
+            <Input label="Halo Wald" />
+            <Input />
+          </div>
+        </div>
+        <div className="button-set">
+          <h3>Helper Text</h3>
+          <div className="button-row">
+            <Input helperText="Halo Wald" />
+            <Input />
+          </div>
+        </div>
+        <div className="button-set">
+          <h3>Full-Width</h3>
+          <div className="button-row">
+            <Input fullWidth />
+            {/* <br /> */}
+            <Input />
+          </div>
+        </div>
+        <div className="button-set">
+          <h3>Multi-line</h3>
+          <div className="button-row">
+            <Input multiline />
+            <Input multiline row={4} />
+          </div>
+        </div>
+        <div className="button-set">
+          <h3>Icons</h3>
+          <div className="button-row">
+            <Input startIcon />
+            <Input endIcon />
+          </div>
+        </div>
+        <div className="button-set">
+          <h3>Error</h3>
+          <div className="button-row">
+            <Input error />
+            <Input error helperText="Helo" />
+            <Input endIcon />
+          </div>
+        </div>
         <footer>
-          created by <a href="https://github.com/Deekshithrathod">Deekshith</a>{" "}
-          - Full Stack
+          created by{" "}
+          <b>
+            <a href="https://github.com/Deekshithrathod">Deekshith</a>
+          </b>{" "}
+          - devChallenges.io
         </footer>
       </div>
     </article>
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,5 +1,132 @@
-const Input = () => {
-  return <div>Input</div>;
+import React, { useState } from "react";
+
+const Input = ({
+  size = "md",
+  label = "Label",
+  helperText = "",
+  placeHolder = "Placeholder",
+  fullWidth = false,
+  multiline = false,
+  row = 1,
+  startIcon = false,
+  endIcon = false,
+  error = false,
+}: {
+  size?: "md" | "sm";
+  placeHolder?: string;
+  label?: string;
+  helperText?: string;
+  fullWidth?: boolean;
+  multiline?: boolean;
+  row?: number;
+  startIcon?: boolean;
+  endIcon?: boolean;
+  error?: boolean;
+}) => {
+  const [isHover, setIsHover] = useState(false);
+  const [isFocus, setIsFocus] = useState(false);
+
+  const handleFocus = (e) => {
+    setIsFocus(true);
+  };
+
+  const handleBlur = (e) => {
+    setIsFocus(false);
+  };
+
+  const inputStyle = {
+    fontFamily: `Noto Sans JP`,
+    fontSize: `14px`,
+    fontStyle: `normal`,
+    fontWeight: 500,
+    lineHeight: `normal`,
+
+    borderRadius: `0.5rem`,
+    border: `1px solid  ${getBorderColor(error, isHover, isFocus)}`,
+
+    width: getWidth(fullWidth),
+    height: getHeight(multiline, row),
+    padding: getPadding(size, startIcon, endIcon),
+  };
+
+  const labelStyle = {
+    color: `${getBorderColor(error, false, true)}`,
+    fontFamily: `Noto Sans JP`,
+    fontSize: `0.75rem`,
+    fontWeight: `400`,
+    marginBottom: `0.25rem`,
+  };
+
+  const containerStyle = {
+    width: getWidth(fullWidth),
+    display: "flex",
+    flexDirection: "column" as `column`,
+  };
+
+  return (
+    <div className="input-container" style={containerStyle}>
+      <label htmlFor="d" style={labelStyle}>
+        {label}
+      </label>
+      {/* {startIcon && <span className="material-symbols-outlined">call</span>} */}
+      <input
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}
+        type="text"
+        placeholder={placeHolder}
+        style={inputStyle}
+      />
+      {/* {endIcon && <span className="material-symbols-outlined">lock</span>} */}
+      {helperText && (
+        <div style={{ ...labelStyle, marginTop: `.25rem` }}>{helperText}</div>
+      )}
+    </div>
+  );
+};
+
+const getPadding = (
+  size: string,
+  startIcon: boolean,
+  endIcon: boolean
+): string => {
+  console.log(`used GetPadding with ${size === "sm"}`);
+
+  let factor = 18;
+  if (size === "sm") {
+    factor = 10;
+  }
+
+  let leftPadding = startIcon ? 3 : 0.75;
+  let rightPadding = endIcon ? 3 : 0.75;
+
+  const finalString = `${factor}px ${rightPadding}rem  ${factor}px ${leftPadding}rem`;
+  console.log(finalString);
+  return finalString;
+};
+
+const getBorderColor = (error: boolean, isHover: boolean, isFocus: boolean) => {
+  if (error) {
+    return isHover ? `#333333` : `#D32F2F`;
+    // return `yellow`;
+  }
+  if (isHover) {
+    return `#333`;
+  }
+  return `#828282`;
+};
+
+const getWidth = (fullWidth: boolean): string => {
+  return fullWidth ? `100%` : `12.5rem`;
+};
+
+const getHeight = (multiline: boolean, row: number) => {
+  let height = 1;
+  if (multiline) {
+    height = row;
+  }
+  return `${height}rem`;
 };
 
 export default Input;
